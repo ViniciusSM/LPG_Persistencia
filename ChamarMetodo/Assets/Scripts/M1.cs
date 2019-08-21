32f37b3542470fbd3096ff258ed642e14b1d9ef0
Vinicius Morisita <vinicius.smorisita@gmail.com>
2019-08-21 10:26:52 -0300

    Chamar metodo por Invoke Reflection

---
+++ ChamarMetodo/Assets/Scripts/M1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M1 : MonoBehaviour
+{
+    string parte1, parte2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parte1 = "meu";
+        parte2 = "Metodo";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Invoke(parte1 + parte2, 0); //( , delay pra executar a ação)
+    }
+
+    void meuMetodo()
+    {
+        transform.Translate(0, 1, 0);
+    }
+}
---
+++ ChamarMetodo/Assets/Scripts/M2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class M2 : MonoBehaviour
+{
+    string parte1, parte2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parte1 = "meu";
+        parte2 = "Metodo";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //StartCoroutine(parte1 + parte2, 1f);
+        MethodInfo metodo = this.GetType().GetMethod(parte1 + parte2, BindingFlags.NonPublic|BindingFlags.Instance);
+        metodo.Invoke(this, new object[] { 1f ,-.1f,"teste"});
+    }
+
+    string meuMetodo(float x, float y, string txt)
+    {
+        transform.Translate(x, y, 0);
+        return "funcionou" + txt;
+    }
+}
