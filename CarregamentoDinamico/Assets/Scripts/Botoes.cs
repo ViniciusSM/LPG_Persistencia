79de73612c76f19e0a2618817498d24ecea723d3
Vinicius Morisita <vinicius.smorisita@gmail.com>
2019-08-28 10:20:16 -0300

    Carregamento Dinamico

---
+++ CarregamentoDinamico/Assets/Scripts/Botoes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botoes : MonoBehaviour
+{
+    public GameObject mSprite;
+
+    ControleSprite cSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cSprite = mSprite.GetComponent<ControleSprite>();
+    }
+
+    public void AcaoBotao1()
+    {
+        cSprite.TrocaTextura("img1");
+    }
+
+    public void AcaoBotao2()
+    {
+        cSprite.TrocaTextura("img2");
+    }
+}
+
---
+++ CarregamentoDinamico/Assets/Scripts/ControleSprite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleSprite : MonoBehaviour
+{
+    Texture2D textura;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TrocaTextura("img1");
+    }
+
+    public void TrocaTextura (string img)
+    {
+        textura = Resources.Load<Texture2D>(img);
+        Sprite sprite = Sprite.Create(textura, new Rect(0, 0, textura.width, textura.height), new Vector2(0.5f, 0.5f));
+
+        GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
